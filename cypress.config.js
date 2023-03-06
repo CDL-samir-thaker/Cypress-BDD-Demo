976feaff5fb17e392719355a32d241a058985ee2
Samir Thaker <46947688+samirthaker2020@users.noreply.github.com>
2023-03-05 20:40:18 -0500

    chore(test): upgrade cypress to 12.6

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-     specPattern: "**/*.feature",
-    async setupNodeEvents(on, config) {
-      // implement node event listeners here
-      const cucumber = require('cypress-cucumber-preprocessor').default
-      on('file:preprocessor', cucumber()) 
-},
-    },
-  });
+    e2e: {
+        specPattern: '**/*.feature',
+        experimentalWebKitSupport: true,
+        async setupNodeEvents(on, config) {
+            // implement node event listeners here
+            const cucumber = require('cypress-cucumber-preprocessor').default;
+            on('file:preprocessor', cucumber());
+        }
+    }
+});
